4363c17c4c867d2914d4aa73e5f9a3150d290109
SyedHamzaHussain000 <syedhamza0015@gmail.com>
2024-11-07 04:07:12 +0500

    live ready

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -1,5 +1,9 @@
 const { notarize } = require('@electron/notarize');
 const { build } = require('../../package.json');
+const dotenv = require('dotenv');
+const { signAsync } = require('@electron/osx-sign');
+
+dotenv.config();
 
 exports.default = async function notarizeMacos(context) {
   const { electronPlatformName, appOutDir } = context;
@@ -27,12 +31,37 @@ exports.default = async function notarizeMacos(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  await notarize({
-    tool: 'notarytool',
-    appBundleId: build.appId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-    teamId: process.env.APPLE_TEAM_ID,
-  });
+  console.log(
+    'reach this ? ...',
+    process.env.APPLE_ID,
+    process.env.APPLE_ID_PASS,
+    process.env.APPLE_TEAM_ID,
+  );
+  try {
+    signAsync({
+      app: '/Users/macbook/electron-Desktop-app/Admin/Admin/release/build/mac-universal/Admin.app',
+      identity: 'Apple Distribution',
+    });
+
+    // signAsync({
+    //   app: '/Users/macbook/electron-Desktop-app/LawFirm/LawFirm_Desktop/release/build/mac-universal/LawFirm.app',
+    //   identity: 'Developer ID Application',
+    //   provisioningProfile: '/Users/macbook/electron-Desktop-app/LawFirm/LawFirm_Desktop/macosdevelopmentcer.provisionprofile'
+    // });
+  } catch (error) {
+    console.log('error: ', error);
+  }
+  try {
+    await notarize({
+      tool: 'notarytool',
+      appBundleId: build.appId,
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASS,
+      teamId: process.env.APPLE_TEAM_ID,
+    });
+    console.log('Notarization complete');
+  } catch (error) {
+    console.error('Notarization failed:', error);
+  }
 };
